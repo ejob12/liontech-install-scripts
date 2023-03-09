134d14a52b535b94c44f0db9230447e362afeffa
liontech <info@liontechacademy.com>
2023-03-08 19:53:26 -0800

    updated jenkins.sh to allow jenkins install on wsl ubuntu

--- jenkins.sh
+++ jenkins.sh
@@ -5,5 +5,12 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+sudo service jenkins start
+sudo service jenkins status
+sudo ufw enable
+
+
+
+#for aws instance
+#sudo systemctl start jenkins
+#sudo systemctl status jenkins
