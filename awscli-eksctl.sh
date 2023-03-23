dac94b9aacb92c14222f2e46efc608a16dc9dc06
liontech <info@liontechacademy.com>
2023-03-23 09:44:48 -0400

    added eks scripts

---
+++ awscli-eksctl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+Verify eksctl version : 
+ eksctl  version 
+
+ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
