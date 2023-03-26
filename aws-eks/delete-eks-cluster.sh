b309965a345adbbe3cb941aec9de3181601ab9eb
liontech <info@liontechacademy.com>
2023-03-26 14:35:48 -0400

    added zip and unzip command

--- aws-eks/delete-eks-cluster.sh
+++ aws-eks/delete-eks-cluster.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+<<<<<<< HEAD
 eksctl delete cluster  liontechprod   --region  ca-central-1 
+=======
+eksctl delete cluster  march25    --region  us-east-1 
+>>>>>>> 60e8766 (added zip and unzip command)
