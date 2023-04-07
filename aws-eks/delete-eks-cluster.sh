796f002f900f7b0743f2ab6fa0802d73207528f7
liontech <info@liontechacademy.com>
2023-04-07 11:27:52 -0400

    added terraform scripts

--- aws-eks/delete-eks-cluster.sh
+++ aws-eks/delete-eks-cluster.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
-eksctl delete cluster  liontechprod   --region  ca-central-1 
-=======
-eksctl delete cluster  march25    --region  us-east-1 
->>>>>>> 60e8766 (added zip and unzip command)
+eksctl delete cluster  liontechstaging    --region  ca-central-1 
---
+++ terraform-install-scripts/terraform-install.md
@@ -0,0 +1,8 @@
+How to install terraform. 
+Install terraform using brew. 
+first make sure you have homebrew install. 
+
+Terraform installation. 
+
+brew install tfenv 
+tfenv list 
