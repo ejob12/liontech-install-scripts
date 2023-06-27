e64c2e00e4e7d3ac0a8ecb072d1553adcb4d32ec
prof prince <prince@liontechacademy.com>
2023-06-27 23:48:34 +0000

    modified script

--- kubeadm/worker.sh
+++ kubeadm/worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #i1) Switch to root user [ sudo -i]
-
-sudo hostnamectl set-hostname  node1
+sudo su    - 
+sudo hostnamectl set-hostname  worker-node1
 
 #2) Disable swap & add kernel settings
 
