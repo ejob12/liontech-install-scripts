3fad74547caa38a27167bd189d6a03eb14a11191
PROF PRINCE <info@liontechacademy.com>
2023-07-07 17:19:33 +0000

    added kubectl install script

--- aws-eks/create-eks-cluster.sh
+++ aws-eks/create-eks-cluster.sh
@@ -1,2 +1,2 @@
 !#/bin/bash
-eksctl create cluster --name banking-clinet  --version 1.22 --region us-east-1 --nodegroup-name mtn --node-type t2.medium --nodes 3 --nodes-min 1 --nodes-max 4 --managed
+eksctl create cluster --name class20devops  --version 1.24 --region us-east-1 --nodegroup-name liontechops --node-type t2.medium --nodes 3 --nodes-min 1 --nodes-max 4 --managed
