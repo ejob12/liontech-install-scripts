be353b83a91b6fe58bc8e3229ac8d22a9ebdea91
liontechacademy <info@liontechacademy.com>
2023-07-13 19:38:47 -0400

    modifed eks create

--- aws-eks/create-eks-cluster.sh
+++ aws-eks/create-eks-cluster.sh
@@ -1,2 +1,4 @@
 !#/bin/bash
 eksctl create cluster --name class20devops  --version 1.24 --region us-east-1 --nodegroup-name liontechops --node-type t2.medium --nodes 3 --nodes-min 1 --nodes-max 4 --managed
+
+eksctl create cluster --name staging  --version 1.27 --region ca-central-1 --nodegroup-name class20-staging --node-type t2.medium --nodes 3 --nodes-min 1 --nodes-max 4 --managed
