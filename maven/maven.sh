aaf75a8a9a794132cec85359511d07708e71370f
ejob12 <ejobjason4@gmail.com>
2024-11-16 01:53:12 +0100

    Update maven.sh

--- maven/maven.sh
+++ maven/maven.sh
@@ -5,9 +5,9 @@ sudo apt install default-jdk   #this installs Java , a dependencyh for maven
 #varify installation of java :  
 java -version
 cd /opt
-sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.tar.gz
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.9-bin.tar.gz
 sudo tar xf apache-maven-*.tar.gz
-sudo ln -s /opt/apache-maven-3.9.1 /opt/maven
+sudo ln -s /opt/apache-maven-3.9.9 /opt/maven
 
 #maven-configuration : 
 #sudo vi /etc/profile.d/maven.sh
@@ -15,8 +15,8 @@ sudo ln -s /opt/apache-maven-3.9.1 /opt/maven
 #copy and paste in the maven.sh: 
 
 #export JAVA_HOME=/usr/lib/jvm/default-java
-#export M2_HOME=/opt/maven
-#export MAVEN_HOME=/opt/maven
+#export M2_HOME=/opt/apache-maven-3.9.9
+#export MAVEN_HOME=/opt/apache-maven-3.9.9
 #export PATH=${M2_HOME}/bin:${PATH}
 
 #Make the script executable with chmod:
